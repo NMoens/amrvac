7da31bc868089a6b93cedd943471faec7795a499
nicolasm <nicolas.moens@student.kuleuven.be>
2019-06-11 16:59:51 +0200

    Trying to add pseudoplanar correction in atmosphere

--- mytests/PseudoPlanarAtmosphere/mod_usr.f
+++ mytests/PseudoPlanarAtmosphere/mod_usr.f
@@ -90,7 +90,8 @@ subroutine initglobaldata_usr
 
   !> Remaining units
   unit_density=(1.d0+4.d0*He_abundance)*mp_cgs*unit_numberdensity
-  unit_pressure=(2.d0+3.d0*He_abundance)*unit_numberdensity*kB_cgs*unit_temperature
+  unit_pressure=(2.d0+3.d0*He_abundance&
+     )*unit_numberdensity*kB_cgs*unit_temperature
   unit_velocity=dsqrt(unit_pressure/unit_density)
   unit_time=unit_length/unit_velocity
   unit_radflux = unit_velocity*unit_pressure
@@ -146,14 +147,16 @@ end subroutine initglobaldata_usr
 !==========================================================================================
 
 !> A routine for specifying initial conditions
-subroutine initial_conditions(ixG^L, ix^L, w, x)
+subroutine initial_conditions(ixGmin1,ixGmin2,ixGmax1,ixGmax2, ixmin1,ixmin2,&
+   ixmax1,ixmax2, w, x)
   use mod_global_parameters
   use mod_constants
   use mod_variables
 
-  integer, intent(in)             :: ixG^L, ix^L
-  double precision, intent(in)    :: x(ixG^S, ndim)
-  double precision, intent(inout) :: w(ixG^S, nw)
+  integer, intent(in)             :: ixGmin1,ixGmin2,ixGmax1,ixGmax2, ixmin1,&
+     ixmin2,ixmax1,ixmax2
+  double precision, intent(in)    :: x(ixGmin1:ixGmax1,ixGmin2:ixGmax2, ndim)
+  double precision, intent(inout) :: w(ixGmin1:ixGmax1,ixGmin2:ixGmax2, nw)
   integer :: i
 
   do i = ixGmin2,ixGmax2
@@ -163,17 +166,21 @@ subroutine initial_conditions(ixG^L, ix^L, w, x)
     w(:,i,r_e) = Er_rk(i)
   enddo
 
-  call get_rad_extravars(w, x, ixG^L, ix^L)
+  call get_rad_extravars(w, x, ixGmin1,ixGmin2,ixGmax1,ixGmax2, ixmin1,ixmin2,&
+     ixmax1,ixmax2)
 
 end subroutine initial_conditions
 
 !==========================================================================================
 
-subroutine boundary_conditions(qt,ixG^L,ixB^L,iB,w,x)
+subroutine boundary_conditions(qt,ixGmin1,ixGmin2,ixGmax1,ixGmax2,ixBmin1,&
+   ixBmin2,ixBmax1,ixBmax2,iB,w,x)
   use mod_global_parameters
-  integer, intent(in)             :: ixG^L, ixB^L, iB
-  double precision, intent(in)    :: qt, x(ixG^S,1:ndim)
-  double precision, intent(inout) :: w(ixG^S,1:nw)
+  integer, intent(in)             :: ixGmin1,ixGmin2,ixGmax1,ixGmax2, ixBmin1,&
+     ixBmin2,ixBmax1,ixBmax2, iB
+  double precision, intent(in)    :: qt, x(ixGmin1:ixGmax1,ixGmin2:ixGmax2,&
+     1:ndim)
+  double precision, intent(inout) :: w(ixGmin1:ixGmax1,ixGmin2:ixGmax2,1:nw)
 
   double precision :: x_vac(ixGmin2:ixGmax2)
   double precision :: rho_vac(ixGmin2:ixGmax2)
@@ -197,8 +204,9 @@ subroutine boundary_conditions(qt,ixG^L,ixB^L,iB,w,x)
   case(4)
     do i = ixBmin2,ixBmax2
       !> Conserve gradE/rho
-      w(ixGmin1:ixGmax1,i,r_e) = w(ixGmin1:ixGmax1,i-1,rho_)/w(ixGmin1:ixGmax1,i-2,rho_) &
-      *(w(ixGmin1:ixGmax1,i-1,r_e) - w(ixGmin1:ixGmax1,i-2,r_e)) + w(ixGmin1:ixGmax1,i-1,r_e)
+      w(ixGmin1:ixGmax1,i,r_e) = w(ixGmin1:ixGmax1,i-1,rho_)/w(ixGmin1:ixGmax1,&
+         i-2,rho_) *(w(ixGmin1:ixGmax1,i-1,r_e) - w(ixGmin1:ixGmax1,i-2,&
+         r_e)) + w(ixGmin1:ixGmax1,i-1,r_e)
       do j = ixGmin1,ixGmax1
         w(j,i,r_e) = min(w(j,i,r_e),w(j,i-1,r_e))
       enddo
@@ -209,18 +217,22 @@ subroutine boundary_conditions(qt,ixG^L,ixB^L,iB,w,x)
   end select
 end subroutine boundary_conditions
 
-subroutine mg_boundary_conditions(qt,ixI^L,ixO^L,iB,w,x)
+subroutine mg_boundary_conditions(qt,ixImin1,ixImin2,ixImax1,ixImax2,ixOmin1,&
+   ixOmin2,ixOmax1,ixOmax2,iB,w,x)
 
   use mod_global_parameters
 
-  integer, intent(in)             :: ixI^L, ixO^L, iB
-  double precision, intent(in)    :: qt, x(ixI^S,1:ndim)
-  double precision, intent(in)    :: w(ixI^S,1:nw)
+  integer, intent(in)             :: ixImin1,ixImin2,ixImax1,ixImax2, ixOmin1,&
+     ixOmin2,ixOmax1,ixOmax2, iB
+  double precision, intent(in)    :: qt, x(ixImin1:ixImax1,ixImin2:ixImax2,&
+     1:ndim)
+  double precision, intent(in)    :: w(ixImin1:ixImax1,ixImin2:ixImax2,1:nw)
 
   double precision :: grad4
 
-  grad4 = sum((w(ixOmin1:ixOmax1,ixOmax2,r_e) - w(ixOmin1:ixOmax1,ixOmax2+1, r_e)) &
-  / (x(ixOmin1:ixOmax1,ixOmax2,2) - x(ixOmin1:ixOmax1,ixOmax2+1,2)))/(ixOmax1-ixOmin1)
+  grad4 = sum((w(ixOmin1:ixOmax1,ixOmax2,r_e) - w(ixOmin1:ixOmax1,ixOmax2+1,&
+      r_e)) / (x(ixOmin1:ixOmax1,ixOmax2,2) - x(ixOmin1:ixOmax1,ixOmax2+1,&
+     2)))/(ixOmax1-ixOmin1)
 
   select case (iB)
     case (1)
@@ -238,8 +250,8 @@ subroutine mg_boundary_conditions(qt,ixI^L,ixO^L,iB,w,x)
 
       ! mg%bc(iB, mg_iphi)%bc_type = mg_bc_neumann
 
-      if (sum(w(ixOmin1:ixOmax1,ixOmax2,r_e)) .le. &
-         sum(w(ixOmin1:ixOmax1,ixOmax2+1, r_e))) then
+      if (sum(w(ixOmin1:ixOmax1,ixOmax2,r_e)) .le. sum(w(ixOmin1:ixOmax1,&
+         ixOmax2+1, r_e))) then
          mg%bc(iB, mg_iphi)%bc_type = mg_bc_neumann
          mg%bc(iB, mg_iphi)%bc_value = zero
       else
@@ -255,56 +267,73 @@ end subroutine mg_boundary_conditions
 !==========================================================================================
 
 !> Calculate gravitational acceleration in each dimension
-subroutine set_gravitation_field(ixI^L,ixO^L,wCT,x,gravity_field)
+subroutine set_gravitation_field(ixImin1,ixImin2,ixImax1,ixImax2,ixOmin1,&
+   ixOmin2,ixOmax1,ixOmax2,wCT,x,gravity_field)
   use mod_global_parameters
-  integer, intent(in)             :: ixI^L, ixO^L
-  double precision, intent(in)    :: x(ixI^S,1:ndim)
-  double precision, intent(in)    :: wCT(ixI^S,1:nw)
-  double precision, intent(out)   :: gravity_field(ixI^S,ndim)
+  integer, intent(in)             :: ixImin1,ixImin2,ixImax1,ixImax2, ixOmin1,&
+     ixOmin2,ixOmax1,ixOmax2
+  double precision, intent(in)    :: x(ixImin1:ixImax1,ixImin2:ixImax2,1:ndim)
+  double precision, intent(in)    :: wCT(ixImin1:ixImax1,ixImin2:ixImax2,1:nw)
+  double precision, intent(out)   :: gravity_field(ixImin1:ixImax1,&
+     ixImin2:ixImax2,ndim)
 
-  double precision :: radius(ixI^S)
+  double precision :: radius(ixImin1:ixImax1,ixImin2:ixImax2)
 
-  radius(ixI^S) = rstar+x(ixI^S,2)*unit_length
+  radius(ixImin1:ixImax1,ixImin2:ixImax2) = rstar+x(ixImin1:ixImax1,&
+     ixImin2:ixImax2,2)*unit_length
 
-  gravity_field(ixI^S,1) = zero
-  gravity_field(ixI^S,2) = -const_G*mstar/(radius(ixI^S))**2*(unit_time**2/unit_length)
+  gravity_field(ixImin1:ixImax1,ixImin2:ixImax2,1) = zero
+  gravity_field(ixImin1:ixImax1,ixImin2:ixImax2,&
+     2) = -const_G*mstar/(radius(ixImin1:ixImax1,&
+     ixImin2:ixImax2))**2*(unit_time**2/unit_length)
 end subroutine set_gravitation_field
 
 !==========================================================================================
 
 !> If defined, this routine is called before writing output, and it can
 !> set/modify the variables in the w array.
-subroutine time_average_values(ixI^L,ixO^L,qt,w,x)
+subroutine time_average_values(ixImin1,ixImin2,ixImax1,ixImax2,ixOmin1,ixOmin2,&
+   ixOmax1,ixOmax2,qt,w,x)
   use mod_global_parameters
   use mod_physics, only: phys_get_pthermal
 
-  integer, intent(in)             :: ixI^L,ixO^L
-  double precision, intent(in)    :: qt,x(ixI^S,1:ndim)
-  double precision, intent(inout) :: w(ixI^S,1:nw)
+  integer, intent(in)             :: ixImin1,ixImin2,ixImax1,ixImax2,ixOmin1,&
+     ixOmin2,ixOmax1,ixOmax2
+  double precision, intent(in)    :: qt,x(ixImin1:ixImax1,ixImin2:ixImax2,&
+     1:ndim)
+  double precision, intent(inout) :: w(ixImin1:ixImax1,ixImin2:ixImax2,1:nw)
 
-  double precision :: pth(ixI^S)
+  double precision :: pth(ixImin1:ixImax1,ixImin2:ixImax2)
 
-  call phys_get_pthermal(w,x,ixI^L,ixO^L,pth)
+  call phys_get_pthermal(w,x,ixImin1,ixImin2,ixImax1,ixImax2,ixOmin1,ixOmin2,&
+     ixOmax1,ixOmax2,pth)
 
-  if (global_time .eq. 1.d0) w(ixI^S,int_rho) = zero
-  if (global_time .eq. 1.d0) w(ixI^S,int_r_e) = zero
-  if (global_time .eq. 1.d0) w(ixI^S,int_p) = zero
-  if (global_time .eq. 1.d0) w(ixI^S,int_m1) = zero
-  if (global_time .eq. 1.d0) w(ixI^S,int_m2) = zero
-  if (global_time .eq. 1.d0) w(ixI^S,int_t) = zero
+  if (global_time .eq. 1.d0) w(ixImin1:ixImax1,ixImin2:ixImax2,int_rho) = zero
+  if (global_time .eq. 1.d0) w(ixImin1:ixImax1,ixImin2:ixImax2,int_r_e) = zero
+  if (global_time .eq. 1.d0) w(ixImin1:ixImax1,ixImin2:ixImax2,int_p) = zero
+  if (global_time .eq. 1.d0) w(ixImin1:ixImax1,ixImin2:ixImax2,int_m1) = zero
+  if (global_time .eq. 1.d0) w(ixImin1:ixImax1,ixImin2:ixImax2,int_m2) = zero
+  if (global_time .eq. 1.d0) w(ixImin1:ixImax1,ixImin2:ixImax2,int_t) = zero
 
-  w(ixI^S,int_rho) = w(ixI^S,int_rho) + w(ixI^S,rho_)*dt
-  w(ixI^S,int_r_e) = w(ixI^S,int_r_e) + w(ixI^S,r_e)*dt
-  w(ixI^S,int_p) = w(ixI^S,int_p) + pth(ixI^S)*dt
-  w(ixI^S,int_m1) = w(ixI^S,int_m1) + w(ixI^S,mom(1))*dt
-  w(ixI^S,int_m2) = w(ixI^S,int_m2) + w(ixI^S,mom(2))*dt
-  w(ixI^S,int_t) = w(ixI^S,int_t) + dt
+  w(ixImin1:ixImax1,ixImin2:ixImax2,int_rho) = w(ixImin1:ixImax1,&
+     ixImin2:ixImax2,int_rho) + w(ixImin1:ixImax1,ixImin2:ixImax2,rho_)*dt
+  w(ixImin1:ixImax1,ixImin2:ixImax2,int_r_e) = w(ixImin1:ixImax1,&
+     ixImin2:ixImax2,int_r_e) + w(ixImin1:ixImax1,ixImin2:ixImax2,r_e)*dt
+  w(ixImin1:ixImax1,ixImin2:ixImax2,int_p) = w(ixImin1:ixImax1,ixImin2:ixImax2,&
+     int_p) + pth(ixImin1:ixImax1,ixImin2:ixImax2)*dt
+  w(ixImin1:ixImax1,ixImin2:ixImax2,int_m1) = w(ixImin1:ixImax1,&
+     ixImin2:ixImax2,int_m1) + w(ixImin1:ixImax1,ixImin2:ixImax2,mom(1))*dt
+  w(ixImin1:ixImax1,ixImin2:ixImax2,int_m2) = w(ixImin1:ixImax1,&
+     ixImin2:ixImax2,int_m2) + w(ixImin1:ixImax1,ixImin2:ixImax2,mom(2))*dt
+  w(ixImin1:ixImax1,ixImin2:ixImax2,int_t) = w(ixImin1:ixImax1,ixImin2:ixImax2,&
+     int_t) + dt
 end subroutine time_average_values
 
 
 
 
-subroutine specialvar_output(ixI^L,ixO^L,w,x,normconv)
+subroutine specialvar_output(ixImin1,ixImin2,ixImax1,ixImax2,ixOmin1,ixOmin2,&
+   ixOmax1,ixOmax2,w,x,normconv)
   ! this subroutine can be used in convert, to add auxiliary variables to the
   ! converted output file, for further analysis using tecplot, paraview, ....
   ! these auxiliary values need to be stored in the nw+1:nw+nwauxio slots
@@ -315,25 +344,38 @@ subroutine specialvar_output(ixI^L,ixO^L,w,x,normconv)
   use mod_physics
   use mod_fld
 
-  integer, intent(in)                :: ixI^L,ixO^L
-  double precision, intent(in)       :: x(ixI^S,1:ndim)
-  double precision                   :: w(ixI^S,nw+nwauxio)
+  integer, intent(in)                :: ixImin1,ixImin2,ixImax1,ixImax2,&
+     ixOmin1,ixOmin2,ixOmax1,ixOmax2
+  double precision, intent(in)       :: x(ixImin1:ixImax1,ixImin2:ixImax2,&
+     1:ndim)
+  double precision                   :: w(ixImin1:ixImax1,ixImin2:ixImax2,&
+     nw+nwauxio)
   double precision                   :: normconv(0:nw+nwauxio)
-  double precision                   :: g_rad(ixI^S,1:ndim), big_gamma(ixI^S)
-  double precision                   :: Tgas(ixI^S),Trad(ixI^S)
+  double precision                   :: g_rad(ixImin1:ixImax1,ixImin2:ixImax2,&
+     1:ndim), big_gamma(ixImin1:ixImax1,ixImin2:ixImax2)
+  double precision                   :: Tgas(ixImin1:ixImax1,ixImin2:ixImax2),&
+     Trad(ixImin1:ixImax1,ixImin2:ixImax2)
   integer                            :: idim
 
   do idim = 1,ndim
-    g_rad(ixO^S,idim) = w(ixO^S,i_op)*w(ixO^S,i_flux(idim))/fld_speedofligt_0
+    g_rad(ixOmin1:ixOmax1,ixOmin2:ixOmax2,idim) = w(ixOmin1:ixOmax1,&
+       ixOmin2:ixOmax2,i_op)*w(ixOmin1:ixOmax1,ixOmin2:ixOmax2,&
+       i_flux(idim))/fld_speedofligt_0
   enddo
-  big_gamma(ixO^S) = g_rad(ixO^S,2)/(const_G*mstar/rstar**2*(unit_time**2/unit_length))
+  big_gamma(ixOmin1:ixOmax1,ixOmin2:ixOmax2) = g_rad(ixOmin1:ixOmax1,&
+     ixOmin2:ixOmax2,2)/(const_G*mstar/rstar**2*(unit_time**2/unit_length))
 
-  call rhd_get_tgas(w, x, ixI^L, ixO^L, Tgas)
-  call rhd_get_trad(w, x, ixI^L, ixO^L, Trad)
+  call rhd_get_tgas(w, x, ixImin1,ixImin2,ixImax1,ixImax2, ixOmin1,ixOmin2,&
+     ixOmax1,ixOmax2, Tgas)
+  call rhd_get_trad(w, x, ixImin1,ixImin2,ixImax1,ixImax2, ixOmin1,ixOmin2,&
+     ixOmax1,ixOmax2, Trad)
 
-  w(ixO^S,nw+1)=Tgas(ixO^S)*unit_temperature
-  w(ixO^S,nw+2)=Trad(ixO^S)*unit_temperature
-  w(ixO^S,nw+3)=big_gamma(ixO^S)
+  w(ixOmin1:ixOmax1,ixOmin2:ixOmax2,nw+1)=Tgas(ixOmin1:ixOmax1,&
+     ixOmin2:ixOmax2)*unit_temperature
+  w(ixOmin1:ixOmax1,ixOmin2:ixOmax2,nw+2)=Trad(ixOmin1:ixOmax1,&
+     ixOmin2:ixOmax2)*unit_temperature
+  w(ixOmin1:ixOmax1,ixOmin2:ixOmax2,nw+3)=big_gamma(ixOmin1:ixOmax1,&
+     ixOmin2:ixOmax2)
 end subroutine specialvar_output
 
 subroutine specialvarnames_output(varnames)
@@ -374,7 +416,7 @@ function get_kappa(p_i,E_i) result(kappa_o)
   akram = 13.1351597305
   bkram = -4.5182188206
 
-  kappa_o = kappa_0*(1.d0+10.d0**akram*rho*(a2/1.d12)**bkram)
+  kappa_o = kappa_0 !*(1.d0+10.d0**akram*rho*(a2/1.d12)**bkram)
 end function
 
 function get_Gamma(p_i,E_i) result(Gamma_o)
@@ -419,43 +461,69 @@ end function
 
 !> Calculate w(iw)=w(iw)+qdt*SOURCE[wCT,qtC,x] within ixO for all indices
 !> iw=iwmin...iwmax.  wCT is at time qCT
-subroutine PseudoPlanar(qdt,ixI^L,ixO^L,iw^LIM,qtC,wCT,qt,w,x)
+subroutine PseudoPlanar(qdt,ixImin1,ixImin2,ixImax1,ixImax2,ixOmin1,ixOmin2,&
+   ixOmax1,ixOmax2,iwmin,iwmax,qtC,wCT,qt,w,x)
   use mod_global_parameters
   use mod_physics, only: phys_get_pthermal
 
-  integer, intent(in)             :: ixI^L, ixO^L, iw^LIM
+  integer, intent(in)             :: ixImin1,ixImin2,ixImax1,ixImax2, ixOmin1,&
+     ixOmin2,ixOmax1,ixOmax2, iwmin,iwmax
   double precision, intent(in)    :: qdt, qtC, qt
-  double precision, intent(in)    :: wCT(ixI^S,1:nw), x(ixI^S,1:ndim)
-  double precision, intent(inout) :: w(ixI^S,1:nw)
+  double precision, intent(in)    :: wCT(ixImin1:ixImax1,ixImin2:ixImax2,1:nw),&
+      x(ixImin1:ixImax1,ixImin2:ixImax2,1:ndim)
+  double precision, intent(inout) :: w(ixImin1:ixImax1,ixImin2:ixImax2,1:nw)
 
-  double precision :: pth(ixI^S)
-  double precision :: radius(ixI^S)
+  double precision :: pth(ixImin1:ixImax1,ixImin2:ixImax2)
+  double precision :: radius(ixImin1:ixImax1,ixImin2:ixImax2)
   integer :: rdir, pdir
 
   rdir = 2
   pdir = 1
 
-  radius(ixI^S) = rstar/unit_length+x(ixI^S,2)
+  radius(ixImin1:ixImax1,ixImin2:ixImax2) = &
+     rstar/unit_length+x(ixImin1:ixImax1,ixImin2:ixImax2,2)
 
   !> Correction for spherical fluxes:
   !> drho/dt = -2 rho v_r/r
-  w(ixI^S,rho_) = w(ixI^S,rho_) - qdt*two*wCT(ixI^S,mom(rdir))/radius(ixI^S)
+  w(ixImin1:ixImax1,ixImin2:ixImax2,rho_) = w(ixImin1:ixImax1,ixImin2:ixImax2,&
+     rho_) - qdt*two*wCT(ixImin1:ixImax1,ixImin2:ixImax2,&
+     mom(rdir))/radius(ixImin1:ixImax1,ixImin2:ixImax2)
 
   !> dm_r/dt = m_phi**2/r
   !> dm_phi/dt = - m_phi m_r/r
-  w(ixI^S,mom(rdir)) = w(ixI^S,mom(rdir)) + qdt*(wCT(ixI^S,mom(pdir)))**two/(radius(ixI^S)*wCT(ixI^S,rho_))
-  w(ixI^S,mom(pdir)) = w(ixI^S,mom(pdir)) - qdt*wCT(ixI^S,mom(rdir))*wCT(ixI^S,mom(pdir))/(radius(ixI^S)*wCT(ixI^S,rho_))
+  w(ixImin1:ixImax1,ixImin2:ixImax2,mom(rdir)) = w(ixImin1:ixImax1,&
+     ixImin2:ixImax2,mom(rdir)) + qdt*(wCT(ixImin1:ixImax1,ixImin2:ixImax2,&
+     mom(pdir)))**two/(radius(ixImin1:ixImax1,&
+     ixImin2:ixImax2)*wCT(ixImin1:ixImax1,ixImin2:ixImax2,rho_))
+  w(ixImin1:ixImax1,ixImin2:ixImax2,mom(pdir)) = w(ixImin1:ixImax1,&
+     ixImin2:ixImax2,mom(pdir)) - qdt*wCT(ixImin1:ixImax1,ixImin2:ixImax2,&
+     mom(rdir))*wCT(ixImin1:ixImax1,ixImin2:ixImax2,&
+     mom(pdir))/(radius(ixImin1:ixImax1,ixImin2:ixImax2)*wCT(ixImin1:ixImax1,&
+     ixImin2:ixImax2,rho_))
 
   !> de/dt = -2 (e+p)v_r/r
-  call phys_get_pthermal(wCT,x,ixI^L,ixO^L,pth)
-  w(ixI^S,e_) = w(ixI^S,e_) - qdt*two*(wCT(ixI^S,e_)+pth(ixI^S))*wCT(ixI^S,mom(rdir))/(wCT(ixI^S,rho_)*radius(ixI^S))
+  call phys_get_pthermal(wCT,x,ixImin1,ixImin2,ixImax1,ixImax2,ixOmin1,ixOmin2,&
+     ixOmax1,ixOmax2,pth)
+  w(ixImin1:ixImax1,ixImin2:ixImax2,e_) = w(ixImin1:ixImax1,ixImin2:ixImax2,&
+     e_) - qdt*two*(wCT(ixImin1:ixImax1,ixImin2:ixImax2,&
+     e_)+pth(ixImin1:ixImax1,ixImin2:ixImax2))*wCT(ixImin1:ixImax1,&
+     ixImin2:ixImax2,mom(rdir))/(wCT(ixImin1:ixImax1,ixImin2:ixImax2,&
+     rho_)*radius(ixImin1:ixImax1,ixImin2:ixImax2))
 
   !> dEr/dt = -2 (E v_r + F_r)/r
   if (rhd_radiation_diffusion) then
-    call get_rad_extravars(w, x, ixI^L, ixO^L)
-    w(ixI^S,r_e) = w(ixI^S,r_e) - qdt*two*(wCT(ixI^S,e_)*wCT(ixI^S,mom(rdir))/wCT(ixI^S,rho_) + w(ixI^S,i_flux(rdir)))/radius(ixI^S)
+    call get_rad_extravars(w, x, ixImin1,ixImin2,ixImax1,ixImax2, ixOmin1,&
+       ixOmin2,ixOmax1,ixOmax2)
+    w(ixImin1:ixImax1,ixImin2:ixImax2,r_e) = w(ixImin1:ixImax1,ixImin2:ixImax2,&
+       r_e) - qdt*two*(wCT(ixImin1:ixImax1,ixImin2:ixImax2,&
+       e_)*wCT(ixImin1:ixImax1,ixImin2:ixImax2,mom(rdir))/wCT(ixImin1:ixImax1,&
+       ixImin2:ixImax2,rho_) + w(ixImin1:ixImax1,ixImin2:ixImax2,&
+       i_flux(rdir)))/radius(ixImin1:ixImax1,ixImin2:ixImax2)
   else
-    w(ixI^S,r_e) = w(ixI^S,r_e) - qdt*two*wCT(ixI^S,e_)*wCT(ixI^S,mom(rdir))/(wCT(ixI^S,rho_)*radius(ixI^S))
+    w(ixImin1:ixImax1,ixImin2:ixImax2,r_e) = w(ixImin1:ixImax1,ixImin2:ixImax2,&
+       r_e) - qdt*two*wCT(ixImin1:ixImax1,ixImin2:ixImax2,&
+       e_)*wCT(ixImin1:ixImax1,ixImin2:ixImax2,mom(rdir))/(wCT(ixImin1:ixImax1,&
+       ixImin2:ixImax2,rho_)*radius(ixImin1:ixImax1,ixImin2:ixImax2))
   endif
 
 end subroutine PseudoPlanar
